6c6000942ea3aebadbf0a1123a1ba12949f88de5
André Canuto <andrecanutoc@gmail.com>
2025-02-02 12:54:16 -0300

    feat: filter by all columns

--- src/utils/playerTable.ts
+++ src/utils/playerTable.ts
@@ -8,7 +8,23 @@ export const ascOrder = ref<boolean>(true)
 
 export const filterData = (data: IPlayer[], searchTerm: string) => {
   if (!searchTerm) return data
-  return data.filter((player) => player.first_name.toLowerCase().includes(searchTerm.toLowerCase()))
+  return data.filter((player) => {
+    const searchTermLower = searchTerm.toLowerCase()
+
+    return Object.keys(player).some((key) => {
+      const value = player[key as keyof IPlayer]
+
+      if (value == null) return false
+
+      if (typeof value === 'object' && value !== null) {
+        return Object.values(value).some(
+          (val) => val?.toString().toLowerCase().includes(searchTermLower) ?? false,
+        )
+      }
+
+      return value.toString().toLowerCase().includes(searchTermLower)
+    })
+  })
 }
 
 export const sortDataValues = (
